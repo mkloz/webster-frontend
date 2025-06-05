22a38f4162a1063b18401c339b72e1ed06af2b37
Mykhailo Kloz <micha21cloz@gmail.com>
2025-06-05 15:11:52 +0100

    fix: tool selection

--- src/modules/home/hooks/use-left-sidebar-store.tsx
+++ src/modules/home/hooks/use-left-sidebar-store.tsx
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 
 import { useShapesStore } from '../../canvas/hooks/shapes-store';
+import { useToolOptionsStore } from '../../canvas/hooks/tool-optios-store';
 
 export type Tools = 'select' | 'pen' | 'eraser' | 'pointer' | 'text' | 'shapes' | 'image';
 
@@ -14,6 +15,19 @@ export const useLeftSidebarStore = create<LeftSidebarStore>((set, get) => ({
   activeTool: 'select',
   setActiveTool: (tool) => {
     const s = useShapesStore.getState();
+    const { setToolOptions } = useToolOptionsStore.getState();
+    if (get().activeTool === tool) {
+      return; // Do nothing if the same tool is selected
+    }
+    if (get().activeTool === 'text') {
+      setToolOptions('text', { selectedTextId: null });
+    }
+    if (get().activeTool === 'shapes') {
+      setToolOptions('shape', { selectedShapeId: null });
+    }
+    if (get().activeTool === 'image') {
+      setToolOptions('image', { selectedImageId: null });
+    }
     const toolsToIgnore = ['select', 'text', 'shapes', 'image'];
     if (get().activeTool !== tool && !toolsToIgnore.includes(tool)) {
       s.clearSelection(); // Clear selection when changing tools
